ea9f7fdff424bfb305ca1016e504843ecfcbc7a0
agungsurya <100355392+asurya-gz@users.noreply.github.com>
2023-10-25 10:09:52 +0700

    Add files via upload

---
+++ opr (2).sql
@@ -0,0 +1,469 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 25 Okt 2023 pada 05.09
+-- Versi server: 8.0.34
+-- Versi PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `opr`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `anggota`
+--
+
+CREATE TABLE `anggota` (
+  `noktp` varchar(16) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `alamat` varchar(255) DEFAULT NULL,
+  `kota` varchar(50) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `no_telp` varchar(15) DEFAULT NULL,
+  `file_ktp` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data untuk tabel `anggota`
+--
+
+INSERT INTO `anggota` (`noktp`, `nama`, `password`, `alamat`, `kota`, `email`, `no_telp`, `file_ktp`) VALUES
+('1234567890123456', 'Anggota Default', 'password123', 'Alamat Default', 'Kota Default', 'anggota@example.com', '123-456-7890', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `buku`
+--
+
+CREATE TABLE `buku` (
+  `idbuku` int NOT NULL,
+  `isbn` varchar(20) NOT NULL,
+  `judul` varchar(255) NOT NULL,
+  `idkategori` int DEFAULT NULL,
+  `pengarang` varchar(100) DEFAULT NULL,
+  `penerbit` varchar(100) DEFAULT NULL,
+  `kota_terbit` varchar(100) DEFAULT NULL,
+  `editor` varchar(100) DEFAULT NULL,
+  `file_gambar` varchar(255) DEFAULT NULL,
+  `tgl_insert` date DEFAULT NULL,
+  `tgl_update` date DEFAULT NULL,
+  `stok` int DEFAULT NULL,
+  `stok_tersedia` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data untuk tabel `buku`
+--
+
+INSERT INTO `buku` (`idbuku`, `isbn`, `judul`, `idkategori`, `pengarang`, `penerbit`, `kota_terbit`, `editor`, `file_gambar`, `tgl_insert`, `tgl_update`, `stok`, `stok_tersedia`) VALUES
+(1, '978-1234567890', 'Buku A', 1, 'Pengarang A', 'Penerbit A', 'Kota Terbit A', 'Editor A', 'gambar_a.jpg', '2023-10-20', '2023-10-24', 100, 96),
+(2, '978-2345678901', 'Buku B', 2, 'Pengarang B', 'Penerbit B', 'Kota Terbit B', 'Editor B', 'gambar_b.jpg', '2023-10-21', '2023-10-24', 75, 75),
+(5, '978-5678901234', 'Buku E', 2, 'Pengarang E', 'Penerbit E', 'Kota Terbit E', 'Editor E', 'gambar_e.jpg', '2023-10-24', '2023-10-24', 110, 110),
+(7, '978-7890123456', 'Buku G', 1, 'Pengarang G', 'Penerbit G', 'Kota Terbit G', 'Editor G', 'gambar_g.jpg', '2023-10-26', '2023-10-24', 70, 70),
+(9, '978-9012345678', 'Buku I', 3, 'Pengarang I', 'Penerbit I', 'Kota Terbit I', 'Editor I', 'gambar_i.jpg', '2023-10-28', '2023-10-24', 95, 95);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_transaksi`
+--
+
+CREATE TABLE `detail_transaksi` (
+  `iddetail` int NOT NULL,
+  `idtransaksi` int DEFAULT NULL,
+  `idbuku` int DEFAULT NULL,
+  `tgl_kembali` date DEFAULT NULL,
+  `denda` int DEFAULT NULL,
+  `idpetugas` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint UNSIGNED NOT NULL,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `idkategori` int NOT NULL,
+  `nama` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `komentar_buku`
+--
+
+CREATE TABLE `komentar_buku` (
+  `idkomentar` int NOT NULL,
+  `idbuku` int DEFAULT NULL,
+  `noktp` varchar(16) DEFAULT NULL,
+  `komentar` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_reset_tokens_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `password_reset_tokens`
+--
+
+CREATE TABLE `password_reset_tokens` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `peminjaman`
+--
+
+CREATE TABLE `peminjaman` (
+  `idtransaksi` int NOT NULL,
+  `tgl_pinjam` date DEFAULT NULL,
+  `isbn` varchar(255) DEFAULT NULL,
+  `peminjam` varchar(255) DEFAULT NULL,
+  `jumlah_pinjam` int DEFAULT NULL,
+  `tanggal_pinjam` date DEFAULT NULL,
+  `tanggal_pengembalian` date DEFAULT NULL,
+  `denda` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data untuk tabel `peminjaman`
+--
+
+INSERT INTO `peminjaman` (`idtransaksi`, `tgl_pinjam`, `isbn`, `peminjam`, `jumlah_pinjam`, `tanggal_pinjam`, `tanggal_pengembalian`, `denda`) VALUES
+(9, '2023-10-24', '978-1234567890', 'User2', 1, '2023-10-24', '2023-10-01', 24000.00),
+(10, '2023-10-24', '978-1234567890', 'User2', 1, NULL, '2023-11-07', 0.00),
+(11, '2023-10-24', '978-1234567890', 'User3', 1, NULL, '2023-11-07', 0.00),
+(12, '2023-10-24', '978-1234567890', 'User3', 1, NULL, '2023-11-07', 0.00);
+
+--
+-- Trigger `peminjaman`
+--
+DELIMITER $$
+CREATE TRIGGER `tr_update_stok_tersedia` AFTER INSERT ON `peminjaman` FOR EACH ROW BEGIN
+    UPDATE buku
+    SET stok_tersedia = stok - NEW.jumlah_pinjam
+    WHERE isbn = NEW.isbn;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` bigint UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `petugas`
+--
+
+CREATE TABLE `petugas` (
+  `idpetugas` int NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data untuk tabel `petugas`
+--
+
+INSERT INTO `petugas` (`idpetugas`, `nama`, `email`, `password`) VALUES
+(1, 'Petugas Default', 'petugas@example.com', 'password123');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `rating_buku`
+--
+
+CREATE TABLE `rating_buku` (
+  `idrating` int NOT NULL,
+  `idbuku` int DEFAULT NULL,
+  `noktp` varchar(16) DEFAULT NULL,
+  `skor_rating` int DEFAULT NULL,
+  `tgl_rating` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `status` int NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `status`) VALUES
+(1, 'User1', 'user1@example.com', '2023-10-22 17:00:00', 'password1', NULL, '2023-10-22 17:00:00', '2023-10-22 17:00:00', 0),
+(2, 'User2', 'user2@example.com', '2023-10-22 17:00:00', 'password2', NULL, '2023-10-22 17:00:00', '2023-10-22 17:00:00', 1),
+(3, 'User3', 'user3@example.com', '2023-10-22 17:00:00', 'password3', NULL, '2023-10-22 17:00:00', '2023-10-22 17:00:00', 1),
+(4, 'User4', 'user4@example.com', '2023-10-22 17:00:00', 'password4', NULL, '2023-10-22 17:00:00', '2023-10-22 17:00:00', 0),
+(5, 'User5', 'user5@example.com', '2023-10-22 17:00:00', 'password5', NULL, '2023-10-22 17:00:00', '2023-10-22 17:00:00', 1),
+(6, 'User6', 'user6@example.com', '2023-10-22 17:00:00', 'password6', NULL, '2023-10-22 17:00:00', '2023-10-22 17:00:00', 0),
+(7, 'User7', 'user7@example.com', '2023-10-22 17:00:00', 'password7', NULL, '2023-10-22 17:00:00', '2023-10-22 17:00:00', 1),
+(8, 'User8', 'user8@example.com', '2023-10-22 17:00:00', 'password8', NULL, '2023-10-22 17:00:00', '2023-10-22 17:00:00', 0),
+(9, 'User9', 'user9@example.com', '2023-10-22 17:00:00', 'password9', NULL, '2023-10-22 17:00:00', '2023-10-22 17:00:00', 0),
+(10, 'User10', 'user10@example.com', '2023-10-22 17:00:00', 'password10', NULL, '2023-10-22 17:00:00', '2023-10-22 17:00:00', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `anggota`
+--
+ALTER TABLE `anggota`
+  ADD PRIMARY KEY (`noktp`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indeks untuk tabel `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`idbuku`),
+  ADD UNIQUE KEY `isbn` (`isbn`);
+
+--
+-- Indeks untuk tabel `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD PRIMARY KEY (`iddetail`);
+
+--
+-- Indeks untuk tabel `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indeks untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`idkategori`);
+
+--
+-- Indeks untuk tabel `komentar_buku`
+--
+ALTER TABLE `komentar_buku`
+  ADD PRIMARY KEY (`idkomentar`);
+
+--
+-- Indeks untuk tabel `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `password_reset_tokens`
+--
+ALTER TABLE `password_reset_tokens`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indeks untuk tabel `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD PRIMARY KEY (`idtransaksi`),
+  ADD KEY `isbn` (`isbn`);
+
+--
+-- Indeks untuk tabel `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indeks untuk tabel `petugas`
+--
+ALTER TABLE `petugas`
+  ADD PRIMARY KEY (`idpetugas`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indeks untuk tabel `rating_buku`
+--
+ALTER TABLE `rating_buku`
+  ADD PRIMARY KEY (`idrating`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `buku`
+--
+ALTER TABLE `buku`
+  MODIFY `idbuku` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT untuk tabel `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  MODIFY `iddetail` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `idkategori` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `komentar_buku`
+--
+ALTER TABLE `komentar_buku`
+  MODIFY `idkomentar` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  MODIFY `idtransaksi` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT untuk tabel `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `petugas`
+--
+ALTER TABLE `petugas`
+  MODIFY `idpetugas` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `rating_buku`
+--
+ALTER TABLE `rating_buku`
+  MODIFY `idrating` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD CONSTRAINT `peminjaman_ibfk_1` FOREIGN KEY (`isbn`) REFERENCES `buku` (`isbn`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
